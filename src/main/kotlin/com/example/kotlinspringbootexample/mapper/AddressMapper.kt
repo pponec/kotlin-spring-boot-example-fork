bd91348d856f2d58e723de80ac0ba4325256a392
pponec <pponec@gmail.com>
2023-09-19 20:39:11 +0200

    Mapstruct

---
+++ src/main/kotlin/com/example/kotlinspringbootexample/mapper/AddressMapper.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinspringbootexample.mapper
+
+import com.example.kotlinspringbootexample.dto.AddressDto
+import com.example.kotlinspringbootexample.entity.Address
+import com.example.kotlinspringbootexample.request.CreateAddressRequest
+import com.example.kotlinspringbootexample.request.UpdateAddressRequest
+import org.mapstruct.Mapper
+import org.mapstruct.MappingConstants
+
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+interface AddressMapper {
+
+    fun toDto(user: Address): AddressDto
+
+    fun toBean(address: AddressDto): Address
+
+    fun toBean(createAddressRequest: CreateAddressRequest): Address
+
+    fun toBean(UpdateAddressRequest: UpdateAddressRequest): Address
+
+}
---
+++ src/main/kotlin/com/example/kotlinspringbootexample/mapper/UserMapper.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinspringbootexample.mapper
+
+import com.example.kotlinspringbootexample.dto.UserDto
+import com.example.kotlinspringbootexample.entity.User
+import com.example.kotlinspringbootexample.request.CreateUserRequest
+import org.mapstruct.Mapper
+import org.mapstruct.MappingConstants
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+interface UserMapper {
+
+    fun toDto(user: User): UserDto
+
+    fun toBean(userDto: UserDto): User
+
+    fun toBean(createUserRequest: CreateUserRequest): User
+
+}
