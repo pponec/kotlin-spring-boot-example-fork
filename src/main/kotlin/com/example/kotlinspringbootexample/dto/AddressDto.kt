d27b1c6cfa32472728536d1eb4c768918540bb68
pponec <pponec@gmail.com>
2023-09-22 19:24:34 +0200

    Fixing

--- src/main/kotlin/com/example/kotlinspringbootexample/dto/AddressDto.kt
+++ src/main/kotlin/com/example/kotlinspringbootexample/dto/AddressDto.kt
@@ -1,10 +1,18 @@
 package com.example.kotlinspringbootexample.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class AddressDto(
+    @Schema( example = "1")
     val id: Long? = null,
+    @Schema( example = "Home")
     val title: String,
+    @Schema( example = "United Kingdom")
     val country: String,
+    @Schema( example = "London, NW1 6XE")
     val city: String,
+    @Schema( example = "221B Baker Street")
     val town: String? = null,
+    @Schema( example = "townhouse")
     val detail: String
 )
