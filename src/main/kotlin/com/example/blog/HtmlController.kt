1a7d02b7ca50e81f68ac9d457476dfb461fc5adf
pponec <pponec@gmail.com>
2023-10-10 18:41:12 +0200

    Rest controller

--- src/main/kotlin/com/example/blog/HtmlController.kt
+++ src/main/kotlin/com/example/blog/HtmlController.kt
@@ -1,12 +1,19 @@
 package com.example.blog
 
-import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller("/")
+@RestController
+@RequestMapping("/")
 class HtmlController {
+
+    @GetMapping("/echo")
+    fun echo(): String {
+        return "blog"
+    }
 
     @GetMapping("/blog")
     fun blog(model: Model): String {
